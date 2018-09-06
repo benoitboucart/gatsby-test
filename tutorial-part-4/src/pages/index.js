4ff97e5ec13d58f438ad0fb0a9accf58ccd8663b
Benoît Boucart <benoit@webbb.be>
2018-09-06 16:15:28 +0200

    tutorial part 4 init

--- tutorial-part-4/src/pages/index.js
+++ tutorial-part-4/src/pages/index.js
@@ -1,12 +1,3 @@
-import React from "react";
-import Layout from "../components/layout";
+import React from "react"
 
-export default () => (
-  <Layout>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-  </Layout>
-);
+export default () => <div>Hello world!</div>
